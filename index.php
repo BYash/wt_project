1231d870b8eff556689ca0b6c3ea5a43bf8fe306
BYash <yash.bhavsar@somaiya.edu>
2017-08-11 22:43:52 +0530

    test2

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<title>SkyNet-Cable & Broadband</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nova+Mono|Patrick+Hand+SC">
 </head>
 	<body>
-		<?php include 'Header/header.html' ?>
+		<?php include 'header/header.html' ?>
 		<h1 id="Heading">Welcome to SkyNet!</h1>
-		<?php include 'Footer/footer.html' ?>
+		<?php include 'footer/footer.html' ?>
 	</body>
 </html>
