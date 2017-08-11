a7de9ba3c96899c6732f3ea9124f75d97ba18227
BYash <yash.bhavsar@somaiya.edu>
2017-08-11 22:33:07 +0530

    test1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nova+Mono|Patrick+Hand+SC">
+</head>
+	<body>
+		<?php include 'Header/header.html' ?>
+		<h1 id="Heading">Welcome to SkyNet!</h1>
+		<?php include 'Footer/footer.html' ?>
+	</body>
+</html>
